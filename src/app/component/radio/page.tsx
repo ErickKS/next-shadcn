5eb2514ecde329bc464f596e4a21b2e82c3da252
Erick <erick.kuwahara79@gmail.com>
2024-01-17 17:08:56 -0300

    refactor: radio component page

--- src/app/component/radio/page.tsx
+++ src/app/component/radio/page.tsx
@@ -1,40 +1,38 @@
 import { Box } from "@/components/layout/box";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
+const radioItems = [
+  {
+    value: "default",
+    label: "Default",
+  },
+  {
+    value: "comfortable",
+    label: "Comfortable",
+  },
+  {
+    value: "compact",
+    label: "Compact",
+  },
+];
+
 export default function RadioPage() {
   return (
     <>
       <Box>
         <RadioGroup defaultValue="default">
-          <div className="flex items-center">
-            <RadioGroupItem value="default" id="default" />
-            <label
-              htmlFor="default"
-              className="pl-2 text-sm font-medium leading-none cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Default
-            </label>
-          </div>
+          {radioItems.map(({ value, label }) => (
+            <div key={value} className="flex items-center">
+              <RadioGroupItem value={value} id={value} />
 
-          <div className="flex items-center">
-            <RadioGroupItem value="comfortable" id="comfortable" />
-            <label
-              htmlFor="comfortable"
-              className="pl-2 text-sm font-medium leading-none cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Comfortable
-            </label>
-          </div>
-
-          <div className="flex items-center">
-            <RadioGroupItem value="compact" id="compact" />
-            <label
-              htmlFor="compact"
-              className="pl-2 text-sm font-medium leading-none cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Compact
-            </label>
-          </div>
+              <label
+                htmlFor={value}
+                className="pl-2 text-sm font-medium leading-none cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                {label}
+              </label>
+            </div>
+          ))}
         </RadioGroup>
       </Box>
     </>
