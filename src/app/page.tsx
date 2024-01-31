1543d28b1794bafd79f0d33705b9826778e5e48c
Erick <erick.kuwahara79@gmail.com>
2024-01-31 18:44:34 -0300

    style: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Box } from "@/components/layout/box";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import clsx from "clsx";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Box>
-      <h1 className="text-xl font-medium">Welcome</h1>
-    </Box>
+    <>
+      <Box>
+        <h1 className="text-xl font-medium">Welcome</h1>
+      </Box>
+
+      <Link
+        href={"https://github.com/ErickKS/next-shadcn"}
+        target="_black"
+        className={clsx(
+          "flex justify-center items-center w-fit gap-2 mt-4 mx-auto py-2 px-4 border rounded-[0.5rem]",
+          "text-sm font-medium",
+          "hover:bg-accent"
+        )}
+      >
+        <GitHubLogoIcon className="h-5 w-5" />
+        Repository
+      </Link>
+    </>
   );
 }
